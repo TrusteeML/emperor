c66ab68044337705664926faf49340b97941a628
asjacobs92 <asjacobs92@inf.ufrgs.br>
2021-11-04 15:04:55 -0300

    Small fixes to os-fingerprinting case and importing scikit-explain directly from github.

--- nprint_case/res/dataset/os-fingerprint/split_pcaps_by_ip.py
+++ nprint_case/res/dataset/os-fingerprint/split_pcaps_by_ip.py
@@ -3,15 +3,15 @@ from os.path import isfile, join
 
 from pcap_splitter.splitter import PcapSplitter
 
-# PCAPS_FOLDER = "../CIC-IDS-2017/pcaps/"
-# SPLITS_FOLDER = "pcaps/"
+PCAPS_FOLDER = "../CIC-IDS-2017/pcaps/"
+SPLITS_FOLDER = "pcaps/"
 
-PCAPS_FOLDER = "pcaps/"
-SPLITS_FOLDER = "pcaps-100k/"
+# PCAPS_FOLDER = "pcaps/"
+# SPLITS_FOLDER = "pcaps-100k/"
 
 for file in listdir(PCAPS_FOLDER):
     if ".pcap" in file and isfile(join(PCAPS_FOLDER, file)):
         print(file)
         ps = PcapSplitter(join(PCAPS_FOLDER, file))
-        # print(ps.split_by_client_ip(SPLITS_FOLDER))
-        print(ps.split_by_count(100000, SPLITS_FOLDER))
+        print(ps.split_by_client_ip(SPLITS_FOLDER))
+        # print(ps.split_by_count(100000, SPLITS_FOLDER))
